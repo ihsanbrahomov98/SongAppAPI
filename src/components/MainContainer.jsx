fffe784ea33e82f166acdd817835c42bfc969f6e
ihsanbrahomov98 <ihsanbrahomov@abv.bg>
2022-07-26 21:05:18 +0300

    documenation added

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,6 +4,8 @@ import { FaSearch } from "react-icons/fa";
 import axios from "axios";
 import { API_URL } from "./API_Url";
 
+// useState with initial data, which was presented in the task image.
+
 const MainContainer = () => {
   const [arrayOfElements, setArrayOfElements] = useState([
     "A",
@@ -12,8 +14,12 @@ const MainContainer = () => {
     "D",
     "E",
   ]);
+
+  // useState with initial input value, which is used for axios GET requests on line 34
+
   const [searchInput, setSearchInput] = useState("");
-  const copyOfArrayOfElements = [...arrayOfElements];
+
+  // useEffect for shifting the items indexes from first to last.
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -23,26 +29,32 @@ const MainContainer = () => {
     return () => clearInterval(interval);
   }, [arrayOfElements]);
 
+  // useEffect for making axios GET requests upon entering new data in the input on line 64, the data is stored in the useState searchInput
+
   useEffect(() => {
     const search = () => {
-      axios.get(`${API_URL}${searchInput}`).then((response) => {
-        const arr = [];
-        const data = response.data.results;
-
-        for (let i = 0; i < data.length; i++) {
-          if (data[i].collectionName.indexOf(searchInput)) {
-            arr.push(data[i].collectionName);
+      axios
+        .get(`${API_URL}${searchInput}${searchInput.length > 0 ? "." : ""}`)
+        .then((response) => {
+          // the reason of putting ${searchInput.length > 0 ? "." : ""} is the following: In the example URL(in the task),
+          // we have https://itunes.apple.com/search?term=radiohead. There is dot after "term=radiohead", but the input in
+          // the next image is just radiohead
+          const arr = [];
+          const data = response.data.results;
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].collectionName.indexOf(searchInput)) {
+              arr.push(data[i].collectionName);
+            }
           }
-        }
-        const uniq = [...new Set(arr)];
-
-        console.log(uniq.sort().slice(0, 5));
-        const sorted = uniq.sort().slice(0, 5);
-        setArrayOfElements(sorted);
-      });
+          const uniqueArray = [...new Set(arr)];
+          const sorted = uniqueArray.sort().slice(0, 5);
+          setArrayOfElements(sorted);
+        });
     };
     search();
   }, [searchInput]);
+
+  const copyOfArrayOfElements = [...arrayOfElements];
 
   return (
     <div className="container">
